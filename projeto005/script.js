3e154d6607a9d0d9aa3f5175433fa993c8bd87c5
thiagomoslavacz <thiagomoslavacz@gmail.com>
2024-01-24 15:24:31 -0300

    Finalizaçâo do projeto005

---
+++ projeto005/script.js
@@ -0,0 +1,23 @@
+function toggleMenu() {
+    var menuWindow = document.getElementById('menu-window-active');
+    var menuBackground = document.querySelector('.menu-background');
+    var body = document.body;
+
+    menuWindow.classList.toggle('menu-window-active');
+
+    var menuIsActive = menuWindow.classList.contains('menu-window-active');
+
+    body.classList.toggle('body-scroll-lock', menuIsActive);
+
+    menuWindow.style.display = menuIsActive ? 'block' : 'none';
+
+    if (menuIsActive) {
+        menuBackground.addEventListener('click', closeMenu);
+    } else {
+        menuBackground.removeEventListener('click', closeMenu);
+    }
+}
+
+function closeMenu() {
+    toggleMenu();
+}
