3bf606448e9f10fe3327510e8e686398c1769754
thiagomoslavacz <thiagomoslavacz@gmail.com>
2024-01-25 17:46:51 -0300

    text indent

--- projeto005/script.js
+++ projeto005/script.js
@@ -4,11 +4,9 @@ function openMenu() {
     var body = document.body;
 
     menuWindow.classList.toggle('menu-window-active');
-
     var menuIsActive = menuWindow.classList.contains('menu-window-active');
 
     body.classList.toggle('body-scroll-lock', menuIsActive);
-
     menuWindow.style.display = menuIsActive ? 'block' : 'none';
 
     if (menuIsActive) {
